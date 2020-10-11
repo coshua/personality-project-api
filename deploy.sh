c6809370029b5469ee101b0f5019baac1b144cba
Dongju <bitlez3470@gmail.com>
2020-10-11 23:58:32 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,15 @@ echo "Deploying to $EB_ENV"
 
 pip install --user --upgrade awsebcli
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region ap-northeast-2
+#aws configure set aws_access_key_id $AWS_ACCESS_KEY
+#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+#aws configure set region ap-northeast-2
 
 # Configure AWS credentials for Elastic Beanstalk
-# mkdir -p ~/.aws
-# echo '[profile eb-cli]' > ~/.aws/config
-# echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
-# echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
+ mkdir -p ~/.aws
+ echo '[profile eb-cli]' > ~/.aws/config
+ echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
+ echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
 eb status
 
 # Deploy application to the appropriate ElasticBeanstalk env
