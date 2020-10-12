5e66a063b75833d2aeed01af9aff764938d81904
Dongju <bitlez3470@gmail.com>
2020-10-12 11:33:13 +0900

    eb list

--- deploy.sh
+++ deploy.sh
@@ -34,6 +34,8 @@ pip install --user --upgrade awsebcli
  echo '[profile eb-cli]' > ~/.aws/config
  echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
  echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
+ echo "eb list"
+ eb list
  echo "eb default"
 eb use production
 echo "eb --version"
